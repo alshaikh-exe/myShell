e2ffbd41909b36acaf04fd70a30417d780ab5495
fatemalbaqlawa <fatemaalbaqlawa@gmail.com>
2026-02-16 14:54:21 +0300

    stage 4 instructions added

--- stage3Integrate.c
+++ stage3Integrate.c
@@ -1,3 +1,12 @@
+/*
+Stage 4:
+1- Implement internal cd command
+2- Handle cd with no arguments (change to HOME directory)
+3- Handle cd with one argument (change to specific path)
+4- Use perror for system errors
+5- Handle error for too many arguments (more than 1 parameter)
+*/
+
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>    // fork
